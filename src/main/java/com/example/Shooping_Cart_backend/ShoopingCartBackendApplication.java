fec2840fb9b4a80bca857f0fcc7ed66f51b68164
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-07 21:41:37 +0530

    first

---
+++ src/main/java/com/example/Shooping_Cart_backend/ShoopingCartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Shooping_Cart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoopingCartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoopingCartBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Shooping_Cart_backend/controller/ShoopingCartController.java
@@ -0,0 +1,6 @@
+package com.example.Shooping_Cart_backend.controller;
+
+public class ShoopingCartController {
+
+
+}
